98b04346e25acc93abc82615c1a52e18b6c169b4
Luiz Henrique <luizh.rocha16@gmail.com>
2024-05-20 11:16:56 -0300

    Primeiro Commit - Projeto Criado

---
+++ src/main/java/com/tceweb/course_sb_mongodb/course_sb_mongodb/CourseSbMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.tceweb.course_sb_mongodb.course_sb_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSbMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSbMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tceweb/course_sb_mongodb/course_sb_mongodb/CourseSbMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tceweb.course_sb_mongodb.course_sb_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSbMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
